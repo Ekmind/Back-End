877cd0457a2cea109c67d205b46a8854667c12a7
Carlos Rodrigo Acosta Domínguez <71846585+Saf-ire@users.noreply.github.com>
2022-06-06 08:59:05 -0600

    v 0.3.1

--- src/middlewares/verifySignup.js
+++ src/middlewares/verifySignup.js
@@ -5,7 +5,7 @@ const validator = require('validator');
 export const checkDuplicatedEmail = async (req, res, next) => {
     const email = await User.findOne({ email: req.body.email });
 
-    if (email) return res.status(400).json({ message: 'Email already in use' })
+    if (email) return res.status(400).json({ message: 'Email already in use' });
 
     next();
 }
@@ -16,7 +16,6 @@ export const checkCredentialsExist = async (req, res, next) => {
     const isEmail = (email) => {
         return validator.default.isEmail(email);
     }
-    //console.log('password:', password.length)
 
     if (!email || !password) return res.status(400).json({ message: 'Email or password missing' });
 
@@ -28,20 +27,8 @@ export const checkCredentialsExist = async (req, res, next) => {
 export const validatePasswordLenght = async (req, res, next) => {
     const password = await req.body.password;
 
-    // console.log(password)
-    // console.log('password:', password.length)
-
     if (password.length < 8) return res.status(403).json({ message: 'Min 8 characters' });
     if (password.length > 24) return res.status(403).json({ message: 'Max 24 characters' });
-
-    next();
-}
-
-export const confirmPassword = async (req, res, next) => {
-    const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
-
-    if (confirmPassword !== password) return res.status(400).json({ message: 'Passwords do not match' });
 
     next();
 }
@@ -57,7 +44,6 @@ export const checkRolesExisted = (req, res, next) => {
             }
         }
     }
-    // console.log(ROLES)
-    // console.log(req.body.role)
+
     next();
 }
